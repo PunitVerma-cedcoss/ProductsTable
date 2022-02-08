f1fd2019ea8e0f1f9666a1c7111a7a46e3b11139
= <=>
2022-02-08 11:05:22 +0530

    edit delete functions

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-const data = []
+var data = []
+data.push({ "ProductId": 1, "ProductName": "iPhone5", "ProductPrice": "210" })
+data.push({ "ProductId": 2, "ProductName": "iPhone6", "ProductPrice": "312" })
+renderTable()
 // hadling form submit
 document.getElementById("submit").addEventListener("click", function (e) {
     e.preventDefault()
@@ -7,13 +10,16 @@ document.getElementById("submit").addEventListener("click", function (e) {
     var ProductPrice = document.getElementById("pp").value
     if (handleError(ProductId, ProductName, ProductPrice)) {
         data.push({ "ProductId": ProductId, "ProductName": ProductName, "ProductPrice": ProductPrice })
-        renderTable({ "ProductId": ProductId, "ProductName": ProductName, "ProductPrice": ProductPrice })
+        renderTable()
     }
     else {
         console.log("some error occured")
     }
 })
-
+// update from click event
+document.getElementById("update").addEventListener("click", function () {
+    updateUtil()
+})
 // error handling
 function handleErrorUtil(ProductId, ProductName, ProductPrice) {
     const errors = []
@@ -52,18 +58,76 @@ function informError(id, error = true) {
 }
 
 // render table with json data
-function renderTable(object) {
-    var tr = generateNodes("tr")
-    tr.appendChild(generateNodes("td", object.ProductId))
-    tr.appendChild(generateNodes("td", object.ProductName))
-    tr.appendChild(generateNodes("td", `USD ${object.ProductPrice}`))
-    document.getElementById("values").prepend(tr)
+function renderTable() {
+    var markup = ''
+    data.forEach(i => {
+        markup += `
+            <tr>
+                <td>${i.ProductId}</td>
+                <td>${i.ProductName}</td>
+                <td>USD ${i.ProductPrice}</td>
+                <td><button onClick="edit('${i.ProductId}')">Edit</button></td>
+                <td><button onClick="del('${i.ProductId}')">Delete</button></td>
+            </tr>
+        `
+    })
+    document.getElementById("values").innerHTML = markup
+}
+// update function
+function updateUtil() {
+    // fetching the values from input fields
+    var ProductId = document.getElementById("pid").value
+    var ProductName = document.getElementById("pn").value
+    var ProductPrice = document.getElementById("pp").value
+    if (handleError(ProductId, ProductName, ProductPrice)) {
+        console.log("updating in the global object  ")
+        // getting the index of the object in array
+        for (let index = 0; index < data.length; index++) {
+            if (data[index].ProductId == ProductId) {
+                console.log("index is " + index)
+                // manipulating the object
+                data[index].ProductId = ProductId
+                data[index].ProductName = ProductName
+                data[index].ProductPrice = ProductPrice
+                break
+            }
+
+        }
+        renderTable()
+        clearFieldsUtil()
+    }
+    else {
+        console.log("errors")
+    }
+
+}
+// editing function
+function edit(id) {
+    alert("id is " + id)
+    // toggling the button from edit to update
+    document.getElementById("submit").classList.add("hide")
+    document.getElementById("update").classList.remove("hide")
+    // getting the data
+    for (let index = 0; index < data.length; index++) {
+        if (data[index].ProductId == id) {
+            console.log(data[index])
+            // populating the form
+            document.getElementById("pid").value = data[index].ProductId
+            document.getElementById("pn").value = data[index].ProductName
+            document.getElementById("pp").value = data[index].ProductPrice
+        }
+
+    }
+}
+// deleting function
+function del(id) {
+    data = data.filter(i => i.ProductId !== parseInt(id))
+    renderTable()
 }
 
-// generate nodes
-function generateNodes(type, content = '') {
-    var node = document.createElement(type)
-    if (content != '')
-        node.innerText = content
-    return node
+// clear all the fields
+function clearFieldsUtil() {
+    document.getElementById("pid").value = ""
+    document.getElementById("pn").value = ""
+    document.getElementById("pp").value = ""
 }
